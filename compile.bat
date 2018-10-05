db1fd1e399853ac227297da7264a4090d0bedec5
Patryk Merchelski <poiu9119@gmail.com>
2018-10-05 08:39:27 +0200

    Added sources

---
+++ compile.bat
@@ -0,0 +1,5 @@
+E:
+cd Python projects
+cd Club Manager
+C:\Python35\Lib\site-packages\PyQt5\pyrcc5.exe .\resources.qrc -o resources_rc.py
+C:\Python35\Lib\site-packages\PyQt5\pyuic5.bat .\gui.ui -o gui.py
